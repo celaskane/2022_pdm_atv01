fad50beafe002a4c8eefa9784e7f6953d7095212
Leandro Assis <leandoru9@gmail.com>
2022-03-25 23:49:04 -0300

    Ajustes realizados conforme requisitos

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 import './styles.css'
 
 const App = () => {
+    const estilos1 = {
+        margin: 'auto',
+        width: 768,
+        height: 255,
+        backgroundColor: '#EEE',
+        padding: 12,
+        borderRadius: 8
+    }
     return (
-        <div style={{margin: 'auto', width: 768, height: 255, 
-        backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+        <div style={estilos1}>
             <p id="texto">Profissionais de saúde</p>
-            <label className="rotulo" htmlFor="none"></label>
+            <div style={{display: 'block', margin: 'auto', 
+            width: '90%', height: '60%', backgroundColor: 'lightgray', 
+            padding: 24, borderRadius: 8}}></div>
         </div>
     )
 }
