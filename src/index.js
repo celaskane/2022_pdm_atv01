3d09c203d4c0e8e93338f7b3ea06db39fc75401a
Leandro Assis <leandoru9@gmail.com>
2022-03-26 16:37:46 -0300

    Estilo dos elementos 1, 2 e 3 concluído. Fotos adicionadas ao projeto.

--- src/index.js
+++ src/index.js
@@ -9,15 +9,18 @@ const App = () => {
         height: 255,
         backgroundColor: '#EEE',
         padding: 12,
-        borderRadius: 8
+        border: 'solid darkgray',
+        borderRadius: 8,
+        borderWidth: 0.5
     }
     return (
         //elemento 1
         <div style={estilos1}>
             <p id="texto">Profissionais de saúde</p>
             <div style={{display: 'block', margin: 'auto', 
-            width: '90%', height: '60%', backgroundColor: 'lightgray', 
-            padding: 24, borderRadius: 8}}>
+            width: '90%', height: '60%', backgroundColor: '#EEE', 
+            padding: 24, border: 'solid lightgray', borderRadius: 8, 
+            borderWidth: 0.1}}>
                 <div className="rotulo1"></div>
                 <div className="rotulo2"></div>
                 <div className="rotulo3"></div>
