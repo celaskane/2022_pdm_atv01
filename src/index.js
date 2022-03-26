9a6acf60b0e54048a5e1f1e5365274120840bd90
celaskane <leandoru9@gmail.com>
2022-03-26 18:24:12 -0300

    Projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './styles.css'
+import './styles.css';
+import img1 from './Images/foto1.jpg'
 
 const App = () => {
     const estilos1 = {
@@ -30,15 +31,18 @@ const App = () => {
             borderWidth: 0.1}}>
                 {/* Elemento 3: Estilo definido com classe CSS definida em um arquivo à parte */}
                 <div className="rotulo1">
-                    <img id="img2" src="" alt="Mr. K"></img>
+                    {/* Primeira imagem acessada da pasta Images usando a instrução import */}
+                    <img id="img" src={img1} alt="András Arató"></img>
                     <p style={{textAlign: 'center'}}>{nomesMedicos.medico1}</p>
                 </div>
                 <div className="rotulo2">
-                    <img id="img3" src="https://tinyurl.com/mv5tb93s" alt="quem é ela?"></img>
+                    {/* Segunda imagem acessada usando um link https comum */}
+                    <img id="img" src="https://tinyurl.com/mv5tb93s" alt="quem é ela?"></img>
                     <p style={{textAlign: 'center'}}>{nomesMedicos.medico2}</p>
                 </div>
                 <div className="rotulo3">
-                    <img id="img2" src={process.env.PUBLIC_URL + '/foto2.jpg'} alt=""/>
+                    {/* Terceira imagem acessada da pasta public usando a propriedade env do objeto global process */}
+                    <img id="img" src={process.env.PUBLIC_URL + '/foto2.jpg'} alt="Hide the pain"/>
                     <p style={{textAlign: 'center'}}>{nomesMedicos.medico3}</p>
                 </div>
             </div>
