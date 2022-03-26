59ddd34f8923728a36815aaf0b963b92e59963d1
celaskane <leandoru9@gmail.com>
2022-03-26 17:46:36 -0300

    Comentários adicionados

--- src/index.js
+++ src/index.js
@@ -13,19 +13,22 @@ const App = () => {
         borderRadius: 8,
         borderWidth: 0.5
     }
+    //Nomes dos médicos definidos em um objeto JSON
     const nomesMedicos = {
         medico1: 'Gregor Samsa',
         medico2: 'Maria Feodorovna',
-        medico3: 'aaa'
+        medico3: 'João Silva'
     }
     return (
-        //elemento 1
+        //Elemento 1: Estilo definido com função que devolve objeto JSON
         <div style={estilos1}>
             <p id="texto">Profissionais de saúde</p>
+            {/* Elemento 2: Estilo definido com CSS In-line */}
             <div style={{display: 'block', margin: 'auto', 
             width: '90%', height: '60%', backgroundColor: '#EEE', 
             padding: 24, border: 'solid lightgray', borderRadius: 8, 
             borderWidth: 0.1}}>
+                {/* Elemento 3: Estilo definido com classe CSS definida em um arquivo à parte */}
                 <div className="rotulo1">
 
                     <p style={{textAlign: 'center'}}>{nomesMedicos.medico1}</p>
